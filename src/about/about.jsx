08f19f8d85f236470f72c9b60d3ae5f19589fdcf
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2020-01-06 14:31:44 +0000

    frontend complete without redux

---
+++ src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='About' small='Us'></PageHeader>
+
+        <h2>Our history</h2>
+        <p>And so it begun.</p>
+        <h2>Mission and Vision</h2>
+        <p>Want to work, need currency.</p>
+    </div>
+)
